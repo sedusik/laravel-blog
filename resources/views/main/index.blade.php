6c749c0a768d4a87ed5c4f30b04a368258218d69
sedusik <sedusova.tanya@yandex.ru>
2024-08-07 18:39:58 +0300

    Add helper asset

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="col-md-4 fetured-post blog-post" data-aos="fade-right">
                     <div class="blog-post-thumbnail-wrapper">
-                        <img src="assets/images/blog_1.jpg" alt="blog post">
+                        <img src="{{ asset('assets/images/blog_1.jpg') }}" alt="blog post">
                     </div>
                     <p class="blog-post-category">Blog post</p>
                     <a href="#!" class="blog-post-permalink">
@@ -18,7 +18,7 @@
                 </div>
                 <div class="col-md-4 fetured-post blog-post" data-aos="fade-up">
                     <div class="blog-post-thumbnail-wrapper">
-                        <img src="assets/images/blog_2.jpg" alt="blog post">
+                        <img src="{{ asset('assets/images/blog_2.jpg') }}" alt="blog post">
                     </div>
                     <p class="blog-post-category">Blog post</p>
                     <a href="#" class="blog-post-permalink">
@@ -27,7 +27,7 @@
                 </div>
                 <div class="col-md-4 fetured-post blog-post" data-aos="fade-left">
                     <div class="blog-post-thumbnail-wrapper">
-                        <img src="assets/images/blog_3.jpg" alt="blog post">
+                        <img src="{{ asset('assets/images/blog_3.jpg') }}" alt="blog post">
                     </div>
                     <p class="blog-post-category">Blog post</p>
                     <a href="#" class="blog-post-permalink">
@@ -42,7 +42,7 @@
                     <div class="row blog-post-row">
                         <div class="col-md-6 blog-post" data-aos="fade-up">
                             <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_4.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_4.jpg') }}" alt="blog post">
                             </div>
                             <p class="blog-post-category">Blog post</p>
                             <a href="#!" class="blog-post-permalink">
@@ -51,27 +51,7 @@
                         </div>
                         <div class="col-md-6 blog-post" data-aos="fade-up">
                             <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_5.jpg" alt="blog post">
-                            </div>
-                            <p class="blog-post-category">Blog post</p>
-                            <a href="#!" class="blog-post-permalink">
-                                <h6 class="blog-post-title">Front becomes an official Instagram Marketing Partner</h6>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row blog-post-row">
-                        <div class="col-md-6 blog-post" data-aos="fade-up">
-                            <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_6.jpg" alt="blog post">
-                            </div>
-                            <p class="blog-post-category">Blog post</p>
-                            <a href="#!" class="blog-post-permalink">
-                                <h6 class="blog-post-title">Front becomes an official Instagram Marketing Partner</h6>
-                            </a>
-                        </div>
-                        <div class="col-md-6 blog-post" data-aos="fade-up">
-                            <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_7.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_5.jpg') }}" alt="blog post">
                             </div>
                             <p class="blog-post-category">Blog post</p>
                             <a href="#!" class="blog-post-permalink">
@@ -82,7 +62,7 @@
                     <div class="row blog-post-row">
                         <div class="col-md-6 blog-post" data-aos="fade-up">
                             <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_8.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_6.jpg') }}" alt="blog post">
                             </div>
                             <p class="blog-post-category">Blog post</p>
                             <a href="#!" class="blog-post-permalink">
@@ -91,7 +71,7 @@
                         </div>
                         <div class="col-md-6 blog-post" data-aos="fade-up">
                             <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_9.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_7.jpg') }}" alt="blog post">
                             </div>
                             <p class="blog-post-category">Blog post</p>
                             <a href="#!" class="blog-post-permalink">
@@ -102,7 +82,7 @@
                     <div class="row blog-post-row">
                         <div class="col-md-6 blog-post" data-aos="fade-up">
                             <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_10.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_8.jpg') }}" alt="blog post">
                             </div>
                             <p class="blog-post-category">Blog post</p>
                             <a href="#!" class="blog-post-permalink">
@@ -111,7 +91,27 @@
                         </div>
                         <div class="col-md-6 blog-post" data-aos="fade-up">
                             <div class="blog-post-thumbnail-wrapper">
-                                <img src="assets/images/blog_11.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_9.jpg') }}" alt="blog post">
+                            </div>
+                            <p class="blog-post-category">Blog post</p>
+                            <a href="#!" class="blog-post-permalink">
+                                <h6 class="blog-post-title">Front becomes an official Instagram Marketing Partner</h6>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row blog-post-row">
+                        <div class="col-md-6 blog-post" data-aos="fade-up">
+                            <div class="blog-post-thumbnail-wrapper">
+                                <img src="{{ asset('assets/images/blog_10.jpg') }}" alt="blog post">
+                            </div>
+                            <p class="blog-post-category">Blog post</p>
+                            <a href="#!" class="blog-post-permalink">
+                                <h6 class="blog-post-title">Front becomes an official Instagram Marketing Partner</h6>
+                            </a>
+                        </div>
+                        <div class="col-md-6 blog-post" data-aos="fade-up">
+                            <div class="blog-post-thumbnail-wrapper">
+                                <img src="{{ asset('assets/images/blog_11.jpg') }}" alt="blog post">
                             </div>
                             <p class="blog-post-category">Blog post</p>
                             <a href="#!" class="blog-post-permalink">
@@ -133,19 +133,19 @@
                             </ol>
                             <div class="carousel-inner" role="listbox">
                                 <figure class="carousel-item active">
-                                    <img src="assets/images/blog_widget_carousel.jpg" alt="First slide">
+                                    <img src="{{ asset('assets/images/blog_widget_carousel.jpg') }}" alt="First slide">
                                     <figcaption class="post-title">
                                         <a href="#!">Front becomes an official Instagram Marketing Partner</a>
                                     </figcaption>
                                 </figure>
                                 <figure class="carousel-item">
-                                    <img src="assets/images/blog_7.jpg" alt="First slide">
+                                    <img src="{{ asset('assets/images/blog_7.jpg') }}" alt="First slide">
                                     <figcaption class="post-title">
                                         <a href="#!">Front becomes an official Instagram Marketing Partner</a>
                                     </figcaption>
                                 </figure>
                                 <div class="carousel-item">
-                                    <img src="assets/images/blog_5.jpg" alt="First slide">
+                                    <img src="{{ asset('assets/images/blog_5.jpg') }}" alt="First slide">
                                     <figcaption class="post-title">
                                         <a href="#!">Front becomes an official Instagram Marketing Partner</a>
                                     </figcaption>
@@ -159,7 +159,7 @@
                     <ul class="post-list">
                         <li class="post">
                             <a href="#!" class="post-permalink media">
-                                <img src="assets/images/blog_widget_1.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_widget_1.jpg') }}" alt="blog post">
                                 <div class="media-body">
                                     <h6 class="post-title">Front becomes an official Instagram Marketing Partner</h6>
                                 </div>
@@ -167,7 +167,7 @@
                         </li>
                         <li class="post">
                             <a href="#!" class="post-permalink media">
-                                <img src="assets/images/blog_widget_2.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_widget_2.jpg') }}" alt="blog post">
                                 <div class="media-body">
                                     <h6 class="post-title">Front becomes an official Instagram Marketing Partner</h6>
                                 </div>
@@ -175,7 +175,7 @@
                         </li>
                         <li class="post">
                             <a href="#!" class="post-permalink media">
-                                <img src="assets/images/blog_widget_3.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_widget_3.jpg') }}" alt="blog post">
                                 <div class="media-body">
                                     <h6 class="post-title">Front becomes an official Instagram Marketing Partner</h6>
                                 </div>
@@ -183,7 +183,7 @@
                         </li>
                         <li class="post">
                             <a href="#!" class="post-permalink media">
-                                <img src="assets/images/blog_widget_4.jpg" alt="blog post">
+                                <img src="{{ asset('assets/images/blog_widget_4.jpg') }}" alt="blog post">
                                 <div class="media-body">
                                     <h6 class="post-title">Front becomes an official Instagram Marketing Partner</h6>
                                 </div>
@@ -193,7 +193,7 @@
                 </div>
                 <div class="widget">
                     <h5 class="widget-title">Categories</h5>
-                    <img src="assets/images/blog_widget_categories.jpg" alt="categories" class="w-100">
+                    <img src="{{ asset('assets/images/blog_widget_categories.jpg') }}" alt="categories" class="w-100">
                 </div>
             </div>
         </div>
