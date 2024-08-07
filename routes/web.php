f13ca923c36bd2870eb20482a373c0aba58fc590
sedusik <sedusova.tanya@yandex.ru>
2024-08-07 17:11:03 +0300

    Add Main/IndexController with view (template Edica)

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', App\Http\Controllers\Main\IndexController::class);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
