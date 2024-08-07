f678ee91d5716adc11ff4190fc6ec898584ec1e5
sedusik <sedusova.tanya@yandex.ru>
2024-08-07 18:49:11 +0300

    Add main page for admin

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->group(function () {
+    Route::prefix('main')->namespace('Main')->group(function () {
+        Route::get('/', 'IndexController');
+    });
+});
