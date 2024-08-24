1ad57fa73da5753063aba2132b0481a594d5d8c0
sedusik <sedusova.tanya@yandex.ru>
2024-08-24 23:32:39 +0300

    Fix: bugs in controllers

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['au
         Route::get('/', 'IndexController')->name('admin.category.index');
         Route::get('/create', 'CreateController')->name('admin.category.create');
         Route::post('/', 'StoreController')->name('admin.category.store');
-        Route::get('/{category}', 'IndexController')->name('admin.category.show');
+        Route::get('/{category}', 'ShowController')->name('admin.category.show');
         Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
         Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
         Route::delete('/{category}', 'DeleteController')->name('admin.category.delete');
@@ -71,7 +71,7 @@ Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['au
         Route::get('/', 'IndexController')->name('admin.tag.index');
         Route::get('/create', 'CreateController')->name('admin.tag.create');
         Route::post('/', 'StoreController')->name('admin.tag.store');
-        Route::get('/{tag}', 'IndexController')->name('admin.tag.show');
+        Route::get('/{tag}', 'ShowController')->name('admin.tag.show');
         Route::get('/{tag}/edit', 'EditController')->name('admin.tag.edit');
         Route::patch('/{tag}', 'UpdateController')->name('admin.tag.update');
         Route::delete('/{tag}', 'DeleteController')->name('admin.tag.delete');
@@ -80,7 +80,7 @@ Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['au
         Route::get('/', 'IndexController')->name('admin.post.index');
         Route::get('/create', 'CreateController')->name('admin.post.create');
         Route::post('/', 'StoreController')->name('admin.post.store');
-        Route::get('/{post}', 'IndexController')->name('admin.post.show');
+        Route::get('/{post}', 'ShowController')->name('admin.post.show');
         Route::get('/{post}/edit', 'EditController')->name('admin.post.edit');
         Route::patch('/{post}', 'UpdateController')->name('admin.post.update');
         Route::delete('/{post}', 'DeleteController')->name('admin.post.delete');
@@ -89,7 +89,7 @@ Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['au
         Route::get('/', 'IndexController')->name('admin.user.index');
         Route::get('/create', 'CreateController')->name('admin.user.create');
         Route::post('/', 'StoreController')->name('admin.user.store');
-        Route::get('/{user}', 'IndexController')->name('admin.user.show');
+        Route::get('/{user}', 'ShowController')->name('admin.user.show');
         Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
         Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
         Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
