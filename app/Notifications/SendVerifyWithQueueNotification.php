0aad26d8d99b47741e7f5b06c65d2830e3db9692
sedusik <sedusova.tanya@yandex.ru>
2024-08-24 23:35:04 +0300

    Refactor: change SendVerifyWithQueueNotification.php

--- app/Notifications/SendVerifyWithQueueNotification.php
+++ app/Notifications/SendVerifyWithQueueNotification.php
@@ -7,50 +7,9 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
 
 class SendVerifyWithQueueNotification extends VerifyEmail implements ShouldQueue
 {
     use Queueable;
-
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
-    public function via($notifiable): array
-    {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail($notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('Подтвердите адрес электронной почты')
-                    ->line('Пожалуйста, нажмите на кнопку ниже, чтобы подтвердить свой адрес электронной почты.')
-                    ->action('Подтвеждение', url('/'))
-                    ->line('Благодарим вас за использование нашего приложения!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
-    }
 }
