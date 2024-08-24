658f8b8652dbda2dd3c31aaeb8a6662b97a33587
sedusik <sedusova.tanya@yandex.ru>
2024-08-24 23:34:18 +0300

    Refactor: add links in views

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -8,6 +8,14 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
+                    <a href="{{ route('main.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-blog"></i>
+                        <p>
+                            Блог
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="{{ route('personal.main.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-home"></i>
                         <p>
@@ -44,6 +52,14 @@
                         <i class="nav-icon fas fa-tags"></i>
                         <p>
                             Тэги
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item mt-3">
+                    <a href="{{ route('admin.main.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-tools"></i>
+                        <p>
+                            Панель администратора
                         </p>
                     </a>
                 </li>
